7da41b0f49ad359ce2bb0e7625e5030e571f0f39
Eric Hop <git@bugfree.be>
2020-07-08 18:02:12 -0700

    Test commit

--- host/hostMap.go
+++ host/hostMap.go
@@ -4,3 +4,20 @@ type HostMap struct {
 	objId int32
 	fields map[int32]interface{}
 }
+
+func (h HostMap) GetInt(keyId int32) int32 {
+	panic("implement me")
+}
+
+func (h HostMap) GetString(keyId int32) string {
+	panic("implement me")
+}
+
+func (h HostMap) SetInt(keyId int32, value int32) {
+	panic("implement me")
+}
+
+func (h HostMap) SetString(keyId int32, value string) {
+	panic("implement me")
+}
+
